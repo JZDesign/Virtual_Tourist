c02841595527292101814284579a5d440aebcdcb
JZDesign <jacobzivandesign@gmail.com>
2017-07-30 14:26:59 -0500

    adding long gesture recognizer
    need to finish long gesture recognizer, then send to flickr display
    page and download images. In that process I need to save the data to CD
    and add a function whenever the MapVC loads to display all pins from CD
    and allow user to tap the current pins.

--- Virtual_Tourist/MapViewController.swift
+++ Virtual_Tourist/MapViewController.swift
@@ -9,20 +9,43 @@
 import UIKit
 import MapKit
 import CoreLocation
+import CoreData
 
 class MapViewController: UIViewController, MKMapViewDelegate, CLLocationManagerDelegate, UIGestureRecognizerDelegate {
+
+    let indicator = ActivityIndicator(text:"Loading")
     
-    @IBOutlet var mapView: MKMapView!
+    var pinAnnotationView:MKPinAnnotationView!
+    var pointAnnotation:MKPointAnnotation!
+    
     
     var annotation:MKAnnotation!
     // tap recognition to add pins
     var tapRecognizer = UITapGestureRecognizer()
+    var longRecognizer = UILongPressGestureRecognizer()
+    // location manager to find user location
+    
     
     var pins = [Pin]()
 
+    
+    
+    @IBOutlet var mapView: MKMapView!
+    
+    var locationManager = CLLocationManager()
+
+    
+    override func viewWillAppear(_ animated: Bool) {
+        self.view.addSubview(indicator)
+        indicator.hide()
+
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
+        longRecognizer = UILongPressGestureRecognizer(target: self, action: #selector(self.longGesture(sender:)))
+        
         // initialize tap
         tapRecognizer = UITapGestureRecognizer(target: self, action: #selector(self.tapGestureHandler(sender:)))
         tapRecognizer.delegate = self
@@ -31,58 +54,78 @@ class MapViewController: UIViewController, MKMapViewDelegate, CLLocationManagerD
     }
 
     // MARK: MAP VIEW
-    func mapView(_ mapView: MKMapView, viewFor annotation: MKAnnotation) -> MKAnnotationView? {
-        
-        let reuseId = "pin"
-        
-        // set annotation data
-        self.annotation = annotation
-        
-        
-        // get location string based on pin geolocation
-        self.geoString {
-            
-            print("OK")
-        }
-        
-        var pinView = mapView.dequeueReusableAnnotationView(withIdentifier: reuseId) as? MKPinAnnotationView
-        // display pin
-        if pinView == nil {
-            pinView = MKPinAnnotationView(annotation: annotation, reuseIdentifier: reuseId)
-            pinView!.canShowCallout = true
-            pinView!.pinTintColor = .green
-            pinView!.rightCalloutAccessoryView = UIButton(type: .detailDisclosure)
-        } else {
-            pinView!.annotation = annotation
-        }
-        
-        pinView?.animatesDrop = true
-        return pinView
-    }
 
     
+    func mapView(_ mapView: MKMapView, viewFor annotation: MKAnnotation) -> MKAnnotationView? {
+        let id = "pin"
+        self.annotation = annotation
+        
+        var pinview = mapView.dequeueReusableAnnotationView(withIdentifier: id) as? MKPinAnnotationView
+        if pinview == nil {
+            pinview = MKPinAnnotationView(annotation: annotation, reuseIdentifier: id)
+            pinview!.canShowCallout = true
+            pinview!.pinTintColor = .green
+            pinview!.rightCalloutAccessoryView = UIButton(type: .detailDisclosure)
+        } else {
+            pinview!.annotation = annotation
+        }
+        
+        let lat = pinview!.annotation?.coordinate.latitude as! Double
+        let lon = pinview!.annotation?.coordinate.longitude as! Double
+        
+        if let context = getContext() {
+            var pin =  NSEntityDescription.insertNewObject(forEntityName: "Pin", into: context) as! Pin
+            pin.latitude = lat
+            pin.longitude = lon 
+            pins.append(pin)
+        }
+        
+        print(pins.count)
+        pinview?.animatesDrop = true
+        return pinview
+    }
+    
+    
+    // This delegate method is implemented to respond to taps. It opens the system browser
+    // to the URL specified in the annotationViews subtitle property.
+    func mapView(_ mapView: MKMapView, annotationView view: MKAnnotationView, calloutAccessoryControlTapped control: UIControl) {
+        if control == view.rightCalloutAccessoryView {
+            print(view.annotation?.coordinate)
+//            let app = UIApplication.shared
+//            if let toOpen = view.annotation?.subtitle! {
+//                
+//                // create mutable object to edit links
+//                var open = toOpen
+//                // remove spaces to prevent crash
+//                if open.range(of: " ") != nil{
+//                    open = open.replacingOccurrences(of: " ", with: "")
+//                }
+//                // set prefix to aid safari
+//                if (open.range(of: "://") != nil){
+//                    app.open(URL(string: open)!, options: [:] , completionHandler: nil)
+//                } else {
+//                    app.open(URL(string: "http://\(open)")!, options: [:] , completionHandler: nil)
+//                }
+            
+            }
+        
+    }
+    
+    
+    
+    
+    
+    // remove current pins
+    
+    func removePins() {
+        self.mapView.removeAnnotations(self.mapView.annotations)
+    }
     
     
     // MARK: GESTURE RECOGNIZER
-    // find location string
-    // modified from https://stackoverflow.com/questions/38977692/how-to-get-location-name-from-default-annotations-mapkit-in-ios
     
-    func geoString(completed: @escaping () -> ())  {
-        //set location
-        let location = CLLocation(latitude: annotation.coordinate.latitude, longitude: annotation.coordinate.longitude)
-        // initialize geocoder to reverse engineer location string
-        let reverseLocation = CLGeocoder()
-        // variable to store result
-        var result = ""
-        reverseLocation.reverseGeocodeLocation(location) { (placemarks, error) in
-            var placemark: CLPlacemark!
-            // read placemark closest to user location
-            placemark = placemarks?[0]
-            print(placemark.addressDictionary)
-            
-            
-            completed()
-        }
+    func longGesture(sender: UILongPressGestureRecognizer? = nil) {
+        
     }
     
     // Tap Recognizer
@@ -95,11 +138,24 @@ class MapViewController: UIViewController, MKMapViewDelegate, CLLocationManagerD
         let pinAnnotation = MKPointAnnotation()
         pinAnnotation.coordinate = pinLocation
         self.annotation = pinAnnotation
+        
         mapView.addAnnotation(self.annotation)
+        //print(annotation.coordinate)
     }
     
     
     
+
+    // MARK: get context for COREDATA
+    
+    func getContext() -> NSManagedObjectContext?  {
+        let delegate = UIApplication.shared.delegate as? AppDelegate
+        if let context = delegate?.persistentContainer.viewContext {
+            return context
+        }
+        print("no Context")
+        return nil
+    }
 
 
 }
